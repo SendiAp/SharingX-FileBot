0b91a59eb1f690a74ca280c8b05ff852d9142f6d
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2026-07-11 22:52:30 +0700

    Update start.py

--- SharingX/modules/start.py
+++ SharingX/modules/start.py
@@ -7,6 +7,7 @@ from SharingX import bot
 
 DATABASE_CHANNEL = -1004437744973
 
+
 @Client.on_message(filters.command("start"))
 async def start(client, message):
     if len(message.command) < 2:
@@ -16,25 +17,91 @@ async def start(client, message):
         token = message.command[1]
         data = base64.urlsafe_b64decode(token).decode()
 
-        if not data.startswith("get-"):
+        if data.startswith("get-"):
+            msg_id = int(data.split("-")[1])
+
+            await client.copy_message(
+                chat_id=message.chat.id,
+                from_chat_id=DATABASE_CHANNEL,
+                message_id=msg_id
+            )
+
+        elif data.startswith("batch-"):
+            _, start_id, end_id = data.split("-")
+
+            for msg_id in range(int(start_id), int(end_id) + 1):
+                try:
+                    await client.copy_message(
+                        chat_id=message.chat.id,
+                        from_chat_id=DATABASE_CHANNEL,
+                        message_id=msg_id
+                    )
+                except:
+                    pass
+        else:
             raise Exception()
 
-        msg_id = int(data.split("-")[1])
+    except Exception as e:
+        await message.reply_text(
+            f"<b>Terjadi Kesalahan:</b> <code>{str(e)}</code>"
+        )
 
-        await client.copy_message(
-            chat_id=message.chat.id,
-            from_chat_id=DATABASE_CHANNEL,
-            message_id=msg_id
+
+@Client.on_message(filters.command("batch") & filters.private)
+async def batch(client, message):
+    try:
+        if len(message.command) != 3:
+            return await message.reply_text(
+                "<b>Penggunaan:</b>\n<code>/batch link_awal link_akhir</code>"
+            )
+
+        start = message.command[1]
+        end = message.command[2]
+
+        if start.isdigit():
+            start_id = int(start)
+        else:
+            start_id = int(start.rstrip("/").split("/")[-1])
+
+        if end.isdigit():
+            end_id = int(end)
+        else:
+            end_id = int(end.rstrip("/").split("/")[-1])
+
+        if start_id > end_id:
+            return await message.reply_text(
+                "<b>ID awal harus lebih kecil dari ID akhir.</b>"
+            )
+
+        data = f"batch-{start_id}-{end_id}"
+        token = base64.urlsafe_b64encode(data.encode()).decode()
+
+        username = (await client.get_me()).username
+        link = f"https://t.me/{username}?start={token}"
+
+        keyboard = InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton(
+                    "Copy Link",
+                    copy_text=link
+                )
+            ]
+        ])
+
+        await message.reply_text(
+            f"<b>Batch Link Berhasil Di Buat :</b>\n\n{link}",
+            reply_markup=keyboard
         )
 
     except Exception as e:
         await message.reply_text(
-            f"<b>Terjadi Kesalahan:</b> `{str(e)}`"
+            f"<b>Terjadi Kesalahan:</b>\n<code>{str(e)}</code>"
         )
+
 
 @Client.on_message(
     filters.private
-    & ~filters.command("start")
+    & ~filters.command("start", "batch")
     & (
         filters.photo
         | filters.text
@@ -84,4 +151,4 @@ async def store_file(client, message):
     except Exception as e:
         await message.reply_text(
             f"<b>Terjadi Kesalahan:</b>\n<code>{str(e)}</code>"
-    )
+        )

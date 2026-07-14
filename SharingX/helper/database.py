46ff4255437ac93c764cd44e43257a1ec9fbe299
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2026-07-14 23:26:15 +0700

    Update database.py

--- SharingX/helper/database.py
+++ SharingX/helper/database.py
@@ -4,139 +4,22 @@ from SharingX.config import MONGO_DB_URL
 mongo = MongoClient(MONGO_DB_URL)
 
 db = mongo["sharingx"]
-forcesub_db = db["forcesub"]
+from pymongo import MongoClient
 
+mongo = MongoClient(MONGO_DB_URI)
+db = mongo["SharingX"]
 
-async def _create():
-    data = forcesub_db.find_one({"_id": "forcesub"})
+forcesubdb = db["forcesub"]
 
-    if not data:
-        data = {
-            "_id": "forcesub",
-            "enabled": False,
-            "channels": []
-        }
-        forcesub_db.insert_one(data)
-
-    return data
-
-
-async def get_forcesub():
-    data = await _create()
-    return data.get("channels", [])
-
-
-async def add_forcesub(chat_id):
-    data = await _create()
-
-    channels = data.get("channels", [])
-
-    if chat_id in channels:
-        return False
-
-    if len(channels) >= 50:
-        return False
-
-    channels.append(chat_id)
-
-    forcesub_db.update_one(
-        {"_id": "forcesub"},
-        {
-            "$set": {
-                "channels": channels
-            }
-        },
+async def add_forcesub(chat_id: int):
+    forcesubdb.update_one(
+        {"_id": chat_id},
+        {"$set": {"chat_id": chat_id}},
         upsert=True
     )
 
-    return True
+async def get_forcesubs():
+    return [x["chat_id"] for x in forcesubdb.find()]
 
-
-async def del_forcesub(chat_id):
-    data = await _create()
-
-    channels = data.get("channels", [])
-
-    if chat_id not in channels:
-        return False
-
-    channels.remove(chat_id)
-
-    forcesub_db.update_one(
-        {"_id": "forcesub"},
-        {
-            "$set": {
-                "channels": channels
-            }
-        },
-        upsert=True
-    )
-
-    return True
-
-
-async def clear_forcesub():
-    forcesub_db.update_one(
-        {"_id": "forcesub"},
-        {
-            "$set": {
-                "channels": []
-            }
-        },
-        upsert=True
-    )
-
-
-async def get_force_sub_status():
-    data = await _create()
-    return data.get("enabled", False)
-
-
-async def set_force_sub_status(status):
-    forcesub_db.update_one(
-        {"_id": "forcesub"},
-        {
-            "$set": {
-                "enabled": bool(status)
-            }
-        },
-        upsert=True
-    )
-
-
-async def get_force_sub_count():
-    data = await _create()
-    return len(data.get("channels", []))
-
-
-async def is_forcesub(chat_id):
-    data = await _create()
-    return chat_id in data.get("channels", [])
-
-
-async def get_force_sub_data():
-    return await _create()
-
-
-async def toggle_force_sub():
-    status = await get_force_sub_status()
-    await set_force_sub_status(not status)
-    return not status
-
-
-async def remove_all_forcesub():
-    forcesub_db.update_one(
-        {"_id": "forcesub"},
-        {
-            "$set": {
-                "channels": [],
-                "enabled": False
-            }
-        },
-        upsert=True
-    )
-
-
-async def total_forcesub():
-    data = await _create()
-    return len(data.get("channels", []))
+async def del_forcesub(chat_id: int):
+    forcesubdb.delete_one({"_id": chat_id})

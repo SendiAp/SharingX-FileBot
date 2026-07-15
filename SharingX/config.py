fc24f480b3675f9493a70ff8076a191bc90c82f0
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2026-07-15 17:00:09 +0700

    Update config.py

--- SharingX/config.py
+++ SharingX/config.py
@@ -8,15 +8,10 @@ from logging.handlers import RotatingFileHandler
 load_dotenv(".env")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "8960778927:AAEqBA9jvqu11YH8jWYCMoUhhuxEFPQ5eaA")
-
 API_ID = getenv("API_ID", "20211998")
-
 BOT_WORKERS = getenv("BOT_WORKERS", "4")
-
 MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://nobek62863:nobek62863@cluster0.7wfjb4p.mongodb.net/?appName=Cluster0")
-
 API_HASH = getenv("API_HASH", "beeeebe74c0c467c47c6ac4a1c9d75b5")
-
 OWNER_ID = [int(x) for x in (os.environ.get("OWNER_ID", "").split())]
 
 LOG_FILE_NAME = "sharingx.txt"

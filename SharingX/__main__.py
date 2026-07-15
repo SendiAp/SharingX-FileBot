5a2ab5d49ff6c60a4a60dc5b23b546eb06f20630
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2026-07-15 16:59:19 +0700

    Update __main__.py

--- SharingX/__main__.py
+++ SharingX/__main__.py
@@ -1,21 +1,41 @@
-import sys
 import asyncio
 import importlib
-
-from SharingX import bot
 from pyrogram import idle
-from SharingX.config import LOGGER
+from pyrogram.errors import RPCError
+
+from SharingX import LOOP, Bot, app, LOGGER
+from SharingX.helper.database import get_bot, remove_bot
+from SharingX.modules import loadModule
+
 
 async def main():
-    try:
-        await bot.start()
-        ex = await bot.get_me()
-        LOGGER("INFO").info(f"{ex.first_name} | [ @{ex.username} ] | 🔥 BERHASIL DIAKTIFKAN! 🔥")
-        await idle()
-    except Exception as a:
-        print(a)
+    await app.start()
+
+    bots = await get_bot()
+
+    if bots:
+        for bt in bots:
+            try:
+                b = Bot(
+                    name=bt["name"],
+                    api_id=bt["api_id"],
+                    api_hash=bt["api_hash"],
+                    bot_token=bt["bot_token"]
+                )
+                await b.start()
+                LOGGER("Bot").info(f"{b.me.first_name} [🔥 BERHASIL DIAKTIFKAN 🔥]")
+            except RPCError:
+                await remove_bot(bt["name"])
+                LOGGER("Bot").warning(f"🗑️ {bt['name']} Dihapus Dari Database.")
+    else:
+        LOGGER("Bot").info("⚠️ Bot Multi Client Tidak Ditemukan.")
+
+    for mod in loadModule():
+        importlib.reload(importlib.import_module(f"Media.modules.{mod}"))
+
+    LOGGER("Bot").info(f"[🔥 BERHASIL DIAKTIFKAN 🔥]")
+    await idle()
+
 
 if __name__ == "__main__":
-    LOGGER("INFO").info("Starting Bot...")
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    LOOP.run_until_complete(main())

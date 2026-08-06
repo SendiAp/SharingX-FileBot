323877c60c8c0e121baf4fc622098cf149bec14f
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2026-08-06 22:16:53 +0700

    Create tools.py

---
+++ SharingX/helper/tools.py
@@ -0,0 +1,16 @@
+import base64
+
+def strtobool(val):
+    return str(val).lower() in ("true", "1", "yes", "y", "on")
+    
+async def encode(string):
+    string_bytes = string.encode("ascii")
+    base64_bytes = base64.urlsafe_b64encode(string_bytes)
+    return (base64_bytes.decode("ascii")).strip("=")
+
+async def decode(text: str):
+    text += "=" * (-len(text) % 4)
+    return base64.urlsafe_b64decode(
+        text.encode()
+    ).decode()
+    

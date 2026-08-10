026cc44de4551f0d8fef0b6277f7afd3ad2656b5
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2026-08-10 18:58:15 +0700

    Update __init__.py

--- SharingX/__init__.py
+++ SharingX/__init__.py
@@ -117,7 +117,7 @@ class MainApp(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            plugins=dict(root="SharingX/modules"),
+            plugins=dict(root="SharingX.modules"),
             workers=4,
         )
 

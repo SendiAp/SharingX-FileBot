8303f8e79dd63efaf1927569da5b1753fafd7f53
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2026-08-11 21:56:39 +0700

    Add help command with usage instructions

--- SharingX/modules/settings_bot.py
+++ SharingX/modules/settings_bot.py
@@ -163,3 +163,40 @@ async def broadcast(client, message):
     )
 
     await pls_wait.edit(status)
+
+@Bot.on_message(filters.command("help") & filters.private & owner_admin)
+async def help_cmd(client, message):
+    text = (
+        "<b>Last Updated: August 11, 2026</b>\n\n"
+
+        "__Beberapa Command Yang Diterapkan Dibot Ini, Harap Fungsikan Dengan Baik Dan Bisa Dimengerti.__\n\n"
+        
+        "<b><u>1. Link Sharing: [OWNER & ADMIN]</b>\n\n"
+        "<b>Function:</b> __Jika Mematikan Link Tidak Terbuat Otomatis, Saat Dalam Mengatur Bot Anda.__\n\n"
+        "<pre>• /link on | <b>Mengaktifkan</b></pre>\n"
+        "<pre>• /link off | <b>Menonaktifkan</b></pre>\n\n"
+
+        "<b><u>2. Logger Database: [OWNER & ADMIN]</b>\n\n"
+        "<b>Function:</b> __Menyimpan Logger Database, Ini Penting Sekali.__\n\n"
+        "<pre>• /adddb | <b>ID - Username - Reply</b></pre>\n"
+        "<pre>• /deldb | <b>ID - Username - Reply</b></pre>\n\n"
+
+        "<b><u>3. Protect File: [OWNER & ADMIN]</b>\n\n"
+        "<b>Function:</b> __Jika Diaktifkan File/Media Yang Anda Buat Tidak Bisa Disimpan, Teruskan, Atau Di Screenshot.__\n\n"
+        "<pre>• /protect true | <b>Mengaktifkan</b></pre>\n"
+        "<pre>• /protect false | <b>Menonaktifkan</b>\n\n"
+
+        "<b><u>4. Admin: [OWNER]</b>\n\n"
+        "<b>Function:</b> __Menambahkan Admin Untuk Menggunakan Beberapa Fitur.__\n\n"
+        "<pre>• /addadmin | <b>ID - Username - Reply</b></pre>\n"
+        "<pre>• /deladmin | <b>ID - Username - Reply</b></pre>\n"
+        "<pre>• /listadmin | <b>Daftar Admin</b></pre>\n\n"
+
+        "<b><u>5. Broadcast: [OWNER]</b>\n\n"
+        "<b>Function:</b> __Mengirimkan Siaran, Seperti Text All Media, Bahakan Tombol.__\n\n"
+        "<pre>• /broadcast | <b>Reply Pesan</b></pre>\n\n"
+
+        "<b>© SharingX</b>"
+    )
+
+    await message.reply_text(text)

346f28625ec41b6c7b51fa67447d37ec4e1c40d5
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2026-08-17 23:35:09 +0700

    Update tools.py

--- SharingX/helper/tools.py
+++ SharingX/helper/tools.py
@@ -1,6 +1,9 @@
+import re
 import base64
 import asyncio
 from pyrogram import filters
+
+from SharingX.helper.database import get_database_channel
 
 async def get_messages(c, message_ids, database_channel):
     messages = []
@@ -32,6 +35,43 @@ async def get_messages(c, message_ids, database_channel):
         messages.extend(msgs)
 
     return messages
+
+async def get_message_id(client, message):
+    db = await get_database_channel(client)
+
+    if not db:
+        return 0
+
+    if message.forward_from_chat and message.forward_from_chat.id == db:
+        return message.forward_from_message_id
+
+    elif message.forward_from_chat or message.forward_sender_name or not message.text:
+        return 0
+
+    else:
+        pattern = r"https://t.me/(?:c/)?(.*)/(\d+)"
+        matches = re.match(pattern, message.text)
+
+        if not matches:
+            return 0
+
+        channel_id = matches[1]
+        msg_id = int(matches[2])
+
+        if channel_id.isdigit():
+            if f"-100{channel_id}" == str(db):
+                return msg_id
+
+        elif message.text.startswith("https://t.me/"):
+            try:
+                chat = await client.get_chat(f"@{channel_id}")
+
+                if chat.id == db:
+                    return msg_id
+            except:
+                pass
+
+    return 0
     
 def strtobool(val):
     return str(val).lower() in ("true", "1", "yes", "y", "on")
